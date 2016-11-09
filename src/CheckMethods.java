8b0a5ff82a7812fedafbac46f816c607d9e22caa
Unknown <Jonathan41@hotmail.com>
2016-11-09 20:32:40 +0000

    added to the method for 2 player use and added a loop that runs 10 times for both players to have 10 moves

--- src/CheckMethods.java
+++ src/CheckMethods.java
@@ -8,17 +8,28 @@ public class CheckMethods {
 
     }
 
-    public static int[][] playerCheck (int[][] board,int column)
+    public static int[][] playerCheck (int[][] board,int column,int player)
     {
         boolean valid = false;
         int num = 0;
 
         while(!valid) {
             valid = false;
-            if(board[num][column-1] == 1){//checking if a piece exists in the spot
-                board[num][column-1] = 2;//adding a piece to the connect four board
-                valid = true;
+            if(player == 1)
+            {
+                if(board[num][column-1] == 1){//checking if a piece exists in the spot
+                    board[num][column-1] = 2;//adding a piece to the connect four board
+                    valid = true;
+                }
             }
+            if(player == 2)
+            {
+                if(board[num][column-1] == 1){//checking if a piece exists in the spot
+                    board[num][column-1] = 2;//adding a piece to the connect four board
+                    valid = true;
+                }
+            }
+
             num++;
         }
 
--- src/TestClass.java
+++ src/TestClass.java
@@ -10,6 +10,7 @@ public class TestClass {
         int [][] board = new int[6][7];
         int column;
         int num = 0;
+        int player;
         boolean valid = false;
         //fills the 2-d array with 1 which signifies a blank space
         for (int i = 0; i < 6; i++) {
@@ -17,12 +18,23 @@ public class TestClass {
                 board[i][j] = 1;
             }
         }
+        for (int i = 0; i < 10; i++) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Please enter a Column: ");
+            column = input.nextInt();
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter a Column: ");
-        column = input.nextInt();
+            player = 1;
 
-        board = CheckMethods.playerCheck(board,column);
+            board = CheckMethods.playerCheck(board,column,player);
+
+            System.out.println("Please enter a Column: ");
+            column = input.nextInt();
+
+            player = 2;
+
+            board = CheckMethods.playerCheck(board,column,player);
+        }
+
 
         for (int i = 5; i >= 0; i--) {//Prints the board out in the console
             for (int j = 0; j < 7; j++) {
