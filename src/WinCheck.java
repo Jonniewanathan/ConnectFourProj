cfad55433302d23982797ae0f4dddc5106187468
Unknown <Jonathan41@hotmail.com>
2016-11-09 20:11:37 +0000

    Added code to place a piece on the board

--- src/WinCheck.java
+++ src/WinCheck.java
@@ -8,6 +8,9 @@ public class WinCheck{
     public static void main(String args[])
     {
         int [][] board = new int[6][7];
+        int column;
+        int num = 0;
+        boolean valid = false;
         //fills the 2-d array with 1 which signifies a blank space
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
@@ -16,6 +19,17 @@ public class WinCheck{
         }
 
         Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a Column: ");
+        column = input.nextInt();
+
+        while(valid != true) {
+            valid = false;
+            if(board[num][column-1] == 1){//checking if a piece exists in the spot
+                board[num][column-1] = 2;//adding a piece to the connect four board
+                valid = true;
+            }
+            num++;
+        }
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
