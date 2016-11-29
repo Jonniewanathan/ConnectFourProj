9dffa37bd8694133d07ef1a0c6559239129371cc
Unknown <Jonathan41@hotmail.com>
2016-11-29 23:02:16 +0000

    Java doc comments add and Javadoc Created

--- src/Player.java
+++ src/Player.java
@@ -1,57 +1,97 @@
 import java.io.*;
 
-/**
- * Created by t00125172 on 15/11/2016.
- */
+/**This is an instantiable Player Class.
+ *@author Jonathan Quirke
+ *@version 1.0*/
 public class Player implements Serializable{
+    //attributes
     private String name;
     private int win;
     private int loses;
 
-    Player()
+    /**no argument constructor method
+     */
+    public Player()
     {
         this("none");
     }
-    Player(String name){
+
+    /**single argument constructor method
+     @param name the full name of the Player
+     */
+    public Player(String name){
         setName(name);
         setExactWin(0);
         setExactLose(0);
     }
+
+    /**mutator method to set the player name
+     * @param name the name of the student
+     */
     public void setName(String name)
     {
         this.name = name;
     }
+
+    /**mutator method to add to the players win count
+     *@param win the number of wins to add to the win count
+     */
     public void setWin(int win)
     {
         this.win += win;
     }
+    /**mutator method to add to the players win count
+     *@param loses the number of loses to add to the lose count
+     */
     public void setLoses(int loses)
     {
         this.loses += loses;
     }
+    /**mutator method to add to the players win count
+     *@param win the numbers of wins the player has
+     */
     public void setExactWin(int win)
     {
         this.win = win;
     }
+    /**mutator method to add to the players win count
+     *@param loses the number of loses the player has
+     */
     public void setExactLose(int loses)
     {
         this.loses = loses;
     }
+
+    /**acessor method to return the player name
+     * @return name the name of the player
+     */
     public String getName()
     {
         return this.name;
     }
+
+    /**acessor method to return the number of player wins
+     * @return win the number of player wins
+     */
     public int getWin()
     {
         return this.win;
     }
+
+    /**acessor method to return the number of player loses
+     * @return loses the number of player loses
+     */
     public int getLoses()
     {
         return this.loses;
     }
+
+    /**toString method to return the players details
+     * @return the name, wins and loses of the player
+     */
     public String toString()
     {
         return "Name: " + getName() + "\n" + "Win: " + getWin() + "\n" + "Loses: " + getLoses();
     }
 
-}
+}//end of class
